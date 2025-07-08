f8ba8a3baed24fe7c05e734518d8141696cd4df1
Nayara N Soares <nnasaria@mundolivre.digital>
2025-07-07 22:31:44 -0300

    Estudando Banco de Dados

---
+++ NodeJs/BancoDeDados-NodeJs/annotations.js
@@ -0,0 +1,53 @@
+/*
+Introdução a Banco de Dados
+
+Dado -> Material bruto
+Informação -> Percepção concreta que se pode extrair dos
+dados.
+
+Um banco de dados é uma coleção organizada de dados.
+
+Por que usar Banco de dados?
+- Facilitar gestão.
+- Acesso e manipulação de dados.
+- Escalabilidade.
+- Eficiência.
+- Organização e Acesso Rápido.
+- Integridade e Confiabilidade.
+
+Banco de Dados Relacional
+Organiza dados em tabelas estruturadas, com linhas e colunas.
+Estabelece relações lógicas entre tabelas com base em chaves
+comuns.
+
+Tabela
+É uma estrutura de armazenamento fundamental em um banco 
+de dados relacional.
+Cada tabela guarda dados de um assunto.
+
+Exemplos:
+Tabela produtos -> Dados de produtos
+Tabela de usuários -> Dados de usuários
+
+Cada tabela é composta por linhas (registros ou tuplas) e
+colunas (campos ou atributos).
+
+Primary Keys
+Chaves primárias identifica exclusivamente cada registro
+em uma tabela. A chave primária garante que cada registro
+seja único dentro da tabela.
+
+SQLite
+É um banco de dados relacional, leve, auto suficiente e 
+totalmente funcional. 
+
+Arquivo único: Todos os dados, incluindo definições de tabelas,
+índices e a própria informação armazenada, são mantidos em
+um único arquivo de disco.
+
+Auto suficiente: O SQLite não depende de nenhuma instalação
+externa ou configuração de servidor, e pode ser incorporado
+diretamente em uma aplicação.
+
+.db -> Convenção para banco de dados.
+*/
