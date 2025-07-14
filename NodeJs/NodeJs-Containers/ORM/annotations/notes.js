ab5c8b56aef62efac4170547e7b0ca99f786db0a
Nayara N Soares <nnasaria@mundolivre.digital>
2025-07-14 10:24:01 -0300

    Estudando ORM

--- NodeJs/NodeJs-Containers/ORM/annotations/notes.js
+++ NodeJs/NodeJs-Containers/ORM/annotations/notes.js
@@ -1,1 +1,28 @@
-/* */
+/* 
+ORM
+ORM é uma ferramenta que facilita o trabalho com banco de
+dados em aplicações, permitindo que interaja com o banco
+sem escrever consultas SQL diretamente. Com o ORM, você
+manipula tabelas e dados do banco como se fossem objetos
+com a linguagem de programação, simplificando a leitura,
+escrita e manutenção do código. Cuida também da conversão
+dos dados entre o banco de dados relacional e os objetos no
+código.
+
+Vantagens
+- Menos código SQL manual
+- Facilita a manutenção
+- Compatível com múltiplos bancos de dados
+
+ORM x Query Builder
+A principal diferença entre eles é o nível de abstração e
+como eles interagem com o banco.
+
+ORM -> Converte tabelas do banco de dados em objetos da
+linguagem de programação.
+
+Query Builder -> Ajuda a criar consultas SQL usando código,
+sem escrever SQL direto.
+
+PRISMA
+*/
---
+++ NodeJs/NodeJs-Containers/README.md
@@ -0,0 +1,5 @@
+# Node.Js - Containers
+
+---
+
+**Autor:** Nayara Nasaria Soares
