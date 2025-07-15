035f137385265dd4e90166cdb308607129aaca1f
Nayara N Soares <nnasaria@mundolivre.digital>
2025-07-14 23:02:21 -0300

    Criando projeto de estudo

---
+++ NodeJs/NodeJs-Containers/Rocketlog-projeto/src/controllers/users-controllers.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+
+class UsersController {
+  create(request: Request, response: Response, next: NextFunction) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
+    return response.json({ message: "ok" });
+  }
+}
+
+export { UsersController };
