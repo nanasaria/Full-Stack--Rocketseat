0337a1eeb59a9b61f510bbbb85a7e5f4e84b9b8c
Nayara N Soares <nnasaria@mundolivre.digital>
2025-07-17 23:36:57 -0300

    Estudando Node.js com TypeScript

--- NodeJs/NodeJs-Containers/Rocketlog-projeto/src/controllers/users-controllers.ts
+++ NodeJs/NodeJs-Containers/Rocketlog-projeto/src/controllers/users-controllers.ts
@@ -1,8 +1,11 @@
 import { Request, Response, NextFunction } from "express";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+import { hash } from "bcrypt";
 import { z } from "zod";
 
 class UsersController {
-  create(request: Request, response: Response, next: NextFunction) {
+  async create(request: Request, response: Response, next: NextFunction) {
     const bodySchema = z.object({
       name: z.string().trim().min(2),
       email: z.string().email(),
@@ -11,7 +14,25 @@ class UsersController {
 
     const { name, email, password } = bodySchema.parse(request.body);
 
-    return response.json({ message: "ok" });
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
+
+    if (userWithSameEmail) {
+      throw new AppError("User with same email already exists");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.status(201).json(userWithoutPassword);
   }
 }
 
